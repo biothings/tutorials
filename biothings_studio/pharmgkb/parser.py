011ee2db3cb899a182765e5d0dd8be8060184aee
Sebastien Lelong <slelong@scripps.edu>
2019-12-27 18:53:15 +0000

    exported code with multiple uploaders

--- biothings_studio/pharmgkb/parser.py
+++ biothings_studio/pharmgkb/parser.py
@@ -1,9 +1,9 @@
 import os, pandas, csv, re
 import numpy as np
 from biothings.utils.dataload import dict_sweep
-
 from biothings import config
 logging = config.logger
+
 
 def load_annotations(data_folder):
     infile = os.path.join(data_folder,"var_drug_ann.tsv")
@@ -11,14 +11,12 @@ def load_annotations(data_folder):
     dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
     results = {}
     for rec in dat:
-
         if not rec["Gene"] or pandas.isna(rec["Gene"]):
             logging.warning("No gene information for annotation ID '%s'", rec["Annotation ID"])
             continue
         _id = re.match(".* \((.*?)\)",rec["Gene"]).groups()[0]
         rec = dict_sweep(rec,vals=[np.nan])
         results.setdefault(_id,[]).append(rec)
-        
     for _id,docs in results.items():
         doc = {"_id": _id, "annotations" : docs}
         yield doc
@@ -38,3 +36,19 @@ def load_druglabels(data_folder):
                            "name" : label_names[i]})
         doc = {"_id" : rec["Gene ID"], "drug_labels" : labels}
         yield doc
+
+
+def load_occurrences(data_folder):
+    infile = os.path.join(data_folder,"occurrences.tsv")
+    assert os.path.exists(infile)
+    dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
+    results = {}
+    for rec in dat:
+        if rec["Object Type"] != "Gene":
+            continue
+        _id = rec["Object ID"]
+        results.setdefault(_id,[]).append(rec)
+    for _id,docs in results.items():
+        doc = {"_id": _id, "occurrences" : docs}
+        yield doc
+
--- biothings_studio/pharmgkb/upload_occurrences.py
+++ biothings_studio/pharmgkb/upload_occurrences.py
@@ -5,20 +5,20 @@ biothings.config_for_app(config)
 
 import biothings.hub.dataload.uploader
 
-from .parser import load_druglabels
+from .parser import load_occurrences
 
 
-class DrugLabelsUploader(biothings.hub.dataload.uploader.BaseSourceUploader):
+class OccurrencesUploader(biothings.hub.dataload.uploader.BaseSourceUploader):
 
     main_source = "pharmgkb"
-    name = "druglabels"
+    name = "occurrences"
     __metadata__ = {"src_meta": {}}
     idconverter = None
     storage_class = biothings.hub.dataload.storage.BasicStorage
 
     def load_data(self, data_folder):
         self.logger.info("Load data from directory: '%s'" % data_folder)
-        return load_druglabels(data_folder)
+        return load_occurrences(data_folder)
 
     @classmethod
     def get_mapping(klass):
