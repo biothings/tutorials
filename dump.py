1e0df3baf583e9950bca310b235ee2be036c2cca
jal347 <linjason.03@gmail.com>
2023-12-14 17:00:05 -0600

    moved files outside of dir

---
+++ dump.py
@@ -0,0 +1,29 @@
+import os
+
+import biothings
+import config
+
+biothings.config_for_app(config)
+import biothings.hub.dataload.dumper
+from biothings.utils.common import uncompressall
+from config import DATA_ARCHIVE_ROOT
+
+
+class PharmgkbDumper(biothings.hub.dataload.dumper.LastModifiedHTTPDumper):
+    SRC_NAME = "pharmgkb"
+    SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
+    SCHEDULE = None
+    UNCOMPRESS = True
+    SRC_URLS = [
+        "https://s3.pgkb.org/data/annotations.zip",
+        "https://s3.pgkb.org/data/drugLabels.zip",
+        "https://s3.pgkb.org/data/occurrences.zip",
+    ]
+    __metadata__ = {"src_meta": {}}
+
+    def post_dump(self, *args, **kwargs):
+        if self.__class__.UNCOMPRESS:
+            self.logger.info(
+                "Uncompress all archive files in '%s'" % self.new_data_folder
+            )
+            uncompressall(self.new_data_folder)
