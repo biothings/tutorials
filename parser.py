6c8451c13b617235bc56ec1bd9eb51cc5a873189
Jason Lin <35415519+jal347@users.noreply.github.com>
2024-01-04 14:58:01 -0800

    fixed squeeze deprecation issue on new versions of pandas

--- parser.py
+++ parser.py
@@ -16,8 +16,8 @@ def load_annotations(data_folder):
     infile = os.path.join(data_folder, "var_drug_ann.tsv")
     assert os.path.exists(infile)
     dat = pandas.read_csv(
-        infile, sep="\t", squeeze=True, quoting=csv.QUOTE_NONE
-    ).to_dict(orient="records")
+        infile, sep="\t", quoting=csv.QUOTE_NONE
+    ).squeeze("columns").to_dict(orient="records")
     results = {}
     for rec in dat:
         if not rec["Gene"] or pandas.isna(rec["Gene"]):
@@ -39,8 +39,8 @@ def load_druglabels(data_folder):
     infile = os.path.join(data_folder, "drugLabels.byGene.tsv")
     assert os.path.exists(infile)
     dat = pandas.read_csv(
-        infile, sep="\t", squeeze=True, quoting=csv.QUOTE_NONE
-    ).to_dict(orient="records")
+        infile, sep="\t", quoting=csv.QUOTE_NONE
+    ).squeeze("columns").to_dict(orient="records")
     for rec in dat:
         label_ids = rec.pop("Label IDs").split(";")
         label_names = rec.pop("Label Names").split(";")
@@ -58,8 +58,8 @@ def load_occurrences(data_folder):
     infile = os.path.join(data_folder, "occurrences.tsv")
     assert os.path.exists(infile)
     dat = pandas.read_csv(
-        infile, sep="\t", squeeze=True, quoting=csv.QUOTE_NONE
-    ).to_dict(orient="records")
+        infile, sep="\t", quoting=csv.QUOTE_NONE
+    ).squeeze("columns").to_dict(orient="records")
     results = {}
     for rec in dat:
         if rec["Object Type"] != "Gene":
