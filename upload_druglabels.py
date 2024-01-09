8d9641e62a61dfe9a2ac40b7282007f4f652f368
Jason Lin <35415519+jal347@users.noreply.github.com>
2024-01-09 15:59:52 -0800

    removed storage class

--- upload_druglabels.py
+++ upload_druglabels.py
@@ -15,7 +15,6 @@ class DrugLabelsUploader(biothings.hub.dataload.uploader.BaseSourceUploader):
     name = "druglabels"
     __metadata__ = {"src_meta": {}}
     idconverter = None
-    storage_class = biothings.hub.dataload.storage.BasicStorage
 
     def load_data(self, data_folder):
         self.logger.info("Load data from directory: '%s'" % data_folder)
