082e9144b45ded48ba5215fad31b8f7c0568c7e1
Jason Lin <35415519+jal347@users.noreply.github.com>
2024-01-09 15:59:34 -0800

    removed storage class

--- upload_annotations.py
+++ upload_annotations.py
@@ -15,7 +15,6 @@ class AnnotationsUploader(biothings.hub.dataload.uploader.BaseSourceUploader):
     name = "annotations"
     __metadata__ = {"src_meta": {}}
     idconverter = None
-    storage_class = biothings.hub.dataload.storage.BasicStorage
 
     def load_data(self, data_folder):
         self.logger.info("Load data from directory: '%s'" % data_folder)
