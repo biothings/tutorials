5cf43602b61721dd8ec9d4bf84de2497222c602f
Jason Lin <35415519+jal347@users.noreply.github.com>
2024-01-09 13:58:55 -0800

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,37 +1,78 @@
-import csv
 import os
-import re
 
-import numpy as np
-import pandas
-from biothings import config
-from biothings.utils.dataload import dict_convert, dict_sweep
+import biothings, config
+biothings.config_for_app(config)
 
-logging = config.logger
+import biothings.hub.dataload.uploader
+
+# when code is exported, import becomes relative
+try:
+    from pharmgkb.parser import load_annotations as parser_func
+except ImportError:
+    from .parser import load_annotations as parser_func
 
 
-def load_annotations(data_folder):
-    infile = os.path.join(data_folder, "var_drug_ann.tsv")
-    assert os.path.exists(infile)
-    dat = pandas.read_csv(
-        infile, sep="\t", squeeze=True, quoting=csv.QUOTE_NONE
-    ).to_dict(orient="records")
-    results = {}
-    for rec in dat:
-        if not rec["Gene"] or pandas.isna(rec["Gene"]):
-            logging.warning(
-                "No gene information for annotation ID '%s'", rec["Annotation ID"]
-            )
-            continue
-        _id = re.match(".* \((.*?)\)", rec["Gene"]).groups()[0]
-        # we'll remove space in keys to make queries easier. Also, lowercase is preferred
-        # for a BioThings API. We'll an helper function from BioThings SDK
-        process_key = lambda k: k.replace(" ", "_").lower()
-        rec = dict_convert(rec, keyfn=process_key)
-        # remove NaN values, not indexable
-        rec = dict_sweep(rec, vals=[np.nan])
-        results.setdefault(_id, []).append(rec)
+class PharmgkbUploader(biothings.hub.dataload.uploader.BaseSourceUploader):
 
-    for _id, docs in results.items():
-        doc = {"_id": _id, "annotations": docs}
-        yield doc
+    name = "pharmgkb"
+    __metadata__ = {"src_meta": {}}
+    idconverter = None
+    storage_class = biothings.hub.dataload.storage.BasicStorage
+
+    def load_data(self, data_folder):
+        self.logger.info("Load data from directory: '%s'" % data_folder)
+        return parser_func(data_folder)
+
+    @classmethod
+    def get_mapping(klass):
+        return         {
+                'annotations': {
+                    'properties': {
+                        'alleles': {
+                            'type': 'text'
+                            },
+                        'annotation_id': {
+                            'type': 'integer'
+                            },
+                        'chemical': {
+                            'type': 'text'
+                            },
+                        'chromosome': {
+                            'normalizer': 'keyword_lowercase_normalizer',
+                            'type': 'keyword'
+                            },
+                        'gene': {
+                            'type': 'text',
+                            'copy_to': [
+                                'all'
+                                ]
+                            },
+                        'notes': {
+                            'type': 'text'
+                            },
+                        'pmid': {
+                            'type': 'integer'
+                            },
+                        'phenotype_category': {
+                            'normalizer': 'keyword_lowercase_normalizer',
+                            'type': 'keyword'
+                            },
+                        'sentence': {
+                            'type': 'text'
+                            },
+                        'significance': {
+                            'type': 'text'
+                            },
+                        'studyparameters': {
+                            'normalizer': 'keyword_lowercase_normalizer',
+                            'type': 'keyword'
+                            },
+                        'variant': {
+                            'type': 'text',
+                            'copy_to': [
+                                'all'
+                                ]
+                            }
+                        }
+                    }
+                }
